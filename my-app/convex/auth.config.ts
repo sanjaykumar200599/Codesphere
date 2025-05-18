87e7aa71b62cca201a0423c8abc48e2ff2554970
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-05-18 13:04:24 +0530

    Modifying the schema.ts

---
+++ my-app/convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain:"https://stunning-molly-51.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +27,10 @@ export default function RootLayout({
   return (
      <ClerkProvider>
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ConvexClientProvider>
+             {children}
+        </ConvexClientProvider>
       </body>
     </html>
     </ClerkProvider>
