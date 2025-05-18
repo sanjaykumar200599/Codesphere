fd05bbc114b10ec0a11981d1d9eea7d6e9d93178
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-05-18 17:15:13 +0530

    Footer is added

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
         <ConvexClientProvider>
              {children}
         </ConvexClientProvider>
+        <Footer/>
       </body>
     </html>
     </ClerkProvider>
