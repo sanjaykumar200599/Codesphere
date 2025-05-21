58d43ed4d524cee5ae3e251d9c5cfe7b7a6f93e6
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-05-22 00:06:46 +0530

    updated the editor panel

--- my-app/src/app/(root)/_components/OutputPanel.tsx
+++ my-app/src/app/(root)/_components/OutputPanel.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function OutputPanel() {
   return (
     <div>OutputPanel</div>
--- my-app/src/app/(root)/page.tsx
+++ my-app/src/app/(root)/page.tsx
@@ -7,9 +7,10 @@ export default function Home(){
     <div className="min-h-screen">
         <div className="max-w-[1800px] mx-auto p-4">
             <Header/>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4"></div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
             <EditorPanel/>
             <OutputPanel/>
+            </div>
         </div>
     
   </div> )
