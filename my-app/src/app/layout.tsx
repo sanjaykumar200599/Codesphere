50a19f607005f82ed86078b424b27765bbda469e
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-05-23 18:49:11 +0530

    Upated the toaster

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
              {children}
         </ConvexClientProvider>
         <Footer/>
+        <Toaster/>
       </body>
     </html>
     </ClerkProvider>
