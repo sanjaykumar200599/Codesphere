80a6f39cc431ffb847172d139c0e6ce63e5b7664
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-05-24 16:38:09 +0530

    modified the Snippets Page

--- my-app/src/app/snippets/[id]/page.tsx
+++ my-app/src/app/snippets/[id]/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client"
 
-function page() {
-  return (
-    <div>page</div>
-  )
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+import SnippetLoadingSkeleton from "./_components/SnippetLoadingSkeleton";
+import NavigationHeader from "@/components/NavigationHeader";
+
+function SnippetDetailPage() {
+    const snippetId= useParams().id;
+    
+    const snippet=useQuery(api.snippets.getSnippetById, { snippetId: snippetId as Id<"snippets">})
+
+    if(snippet === undefined) return <SnippetLoadingSkeleton/>
+    return <div className="min h-screen bg-[#0a0a0f]">
+        <NavigationHeader/>
+        
+    </div>
+
+
 }
-
-export default page
+export default SnippetDetailPage
